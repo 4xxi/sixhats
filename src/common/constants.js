3ecf7814af7262064752c5f33e6e4aea98083b76
Sergey Alekseyev <sa@4xxi.com>
2019-10-04 17:40:26 +0600

    Removed creds

--- src/common/constants.js
+++ src/common/constants.js
@@ -28,11 +28,4 @@ export const hats = {
 };
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyCqXlG2OKFafIAccp34os5vSx_bBZPzdsM",
-  authDomain: "sixhats-b824b.firebaseapp.com",
-  databaseURL: "https://sixhats-b824b.firebaseio.com",
-  projectId: "sixhats-b824b",
-  storageBucket: "",
-  messagingSenderId: "309633110370",
-  appId: "1:309633110370:web:02a38699b85bbd3ee0dc3b"
 };
